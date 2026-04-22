16bf5f37a13fcbbfe8beea38b1831549f64fae8d
RemixSheep <rahmandwi660@gmail.com>
2026-04-23 00:04:42 +0700

    Create halaman.php

---
+++ Pekan 09/062_Rahman Dwi I/halaman.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman PHP</title>
+</head>
+<body>
+
+<h1>Belajar PHP Dasar</h1>
+
+<p>Ini adalah contoh kombinasi HTML dan PHP dalam satu file.</p>
+
+<?php
+echo "Baris 1: Halo, saya sedang belajar PHP <br>";
+echo "Baris 2: PHP adalah bahasa server-side <br>";
+echo "Baris 3: PHP bisa digabung dengan HTML <br>";
+?>
+
+</body>
+</html>
