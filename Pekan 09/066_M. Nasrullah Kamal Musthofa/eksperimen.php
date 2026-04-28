279855ee06a92792eaa31ce800bf9d981ec0b30c
nasrul-cyber <mnasrullahkamalm@gmail.com>
2026-04-28 11:26:06 +0700

    Add PHP experiment page with personal data

---
+++ Pekan 09/066_M. Nasrullah Kamal Musthofa/eksperimen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+<h1>Data Diri Mahasiswa</h1>
+<hr>
+
+<?php
+echo "Nama: M. Nasrullah Kamal Musthofa <br>";
+echo "Universitas: Universitas Trunodjoyo Madura <br>";
+echo "Hobi: Gaming dan Menulis <br>";
+echo "Cita-cita: Menjadi Web Developer <br>";
+echo "Alasan belajar Web Programming: Untuk membuat aplikasi dan website sendiri <br>";
+?>
+
+</body>
+</html>
