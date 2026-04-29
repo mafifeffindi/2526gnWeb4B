bf84c2a970a8bae63e9cad2536e0b9a18d550518
ahmddhani659-cmyk <ahmddhani659@gmail.com>
2026-04-29 08:45:34 +0700

    Add files via upload

---
+++ Pekan 10/050_Dhani/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Latihan</title>
+</head>
+<body>
+    <h1>belajar PHP</h1>
+    <?php
+    echo "hari ini ssya belajar server side programing";
+    ?>
+    </body>
+    </html>
