40404c94e52e1a0ca6bb68108fdf34d50a611659
20rizkyfauziaputri <20rizkyfauziaputri@gmail.com>
2026-04-29 08:49:58 +0700

    Create eksperimen.php

---
+++ Pekan 10/072_rizki fauzia putri/eksperimen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ekperimen PHP</title>
+</head>
+<body>
+    <h1>Data Diri Mahasiswa</h1>
+    <hr>
+    <?php
+    echo "Nama:rizki fauzia<br>";
+    echo "Universitas Trunojoyo madura<br>";
+    echo "Nim:240631100072<br>";
+    echo "sosial media<br>";
+    echo "instagram: ririsaaaa_<br>";
+    ?>
+    </body>
+    </html>
+
