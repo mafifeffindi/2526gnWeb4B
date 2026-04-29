0a550a2980f8920bd2ddf2164b549da315d11cd9
Mohammad Hamzah <hamzah.ap001@gmail.com>
2026-04-29 08:41:49 +0700

    Add files via upload

---
+++ Pekan 10/064. Hamzah/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Latihan HTML dan PHP</title>
+</head>
+<body>
+
+<h1>Belajar Kombinasi HTML dan PHP</h1>
+
+<?php
+echo "<p>Hari ini saya belajar server-side programming menggunakan PHP.</p>";
+echo "<p>Saya bisa menggabungkan HTML dan PHP dalam satu file.</p>";
+echo "<p>PHP diproses di server dan hasilnya dikirim sebagai HTML.</p>";
+?>
+
+</body>
+</html>
