30ca06c5bc93c7091c66b1ce588ff9e3adf1ab99
kamilahnovidatul054 <kamilahnovidatul18@gmail.com>
2026-04-29 08:42:55 +0700

    Add files via upload
    Pekan 10 Novi

---
+++ Pekan 10/054_Novidatul/tugasnov.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <title>eksperimen php</title>
+</head>
+<body>
+    <h1>Data diri Mahasiswa</h1>
+    <hr>
+    <?php
+    echo"Nama: Novidatul<br>";
+    echo"Universitas : UTM<br>";
+    echo"Hobi : Olahraga<br>";
+    echo"Cita-cita : Teacher<br>";
+    echo"Alasan belajar web : menambah skill<br>";
+    ?>
+</body>
+</html>
