1e047e7fdc80244e72655c535d602e07df3c07aa
nurhasanah067 <nurhasanah200306@gmail.com>
2026-04-29 08:49:09 +0700

    Add files via upload
    TUGAS PEKAN 10 KHANZA

---
+++ Pekan 10/067_NUR HASANAH/elong10.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Halaman php</title>
+</head>
+<body>
+    <H1>SELAMAT DATANG</H1>
+    <p> Ini Adalah Halaman Sederhana Gabungan HTML dan PHP</P>
+    <?php
+    echo"Nama : Nur Hasanah<br>";
+    echo"Jurusan : Kedokteran<br>";
+    echo"SEMANGAT BELAJAR PHP!<br>";
+    ?>
+    </body>
+    </html>
