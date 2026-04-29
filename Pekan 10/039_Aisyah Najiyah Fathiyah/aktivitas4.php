9e956d4e6b91aff94923a27007f500bae2b7919d
fathiyahaisyahnajiyah <fathiyahaisyahnajiyah@gmail.com>
2026-04-29 08:38:26 +0700

    Create aktivitas4.php

---
+++ Pekan 10/039_Aisyah Najiyah Fathiyah/aktivitas4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>halaman php</title>
+</head>
+<body>
+
+<h1>Selamat datang</h1>
+<p>Ini adalah halaman sederhana gabungan html dan php.</p>
+
+<?php
+echo "Nama : Aisyah Najiyah Fathiyah <br>";
+echo "Jurusan : Pendidikan Informatika <br>";
+echo "Semangat belajar PHP ! <br>";
+?>
+
+</body>
+</html>
