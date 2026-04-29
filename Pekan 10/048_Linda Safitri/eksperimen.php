8d3e755238fe4c861b3cebcee45bc15634ece3e0
lindasafitriii <safitrilinda2006@gmail.com>
2026-04-29 08:45:11 +0700

    Create eksperimen.php

---
+++ Pekan 10/048_Linda Safitri/eksperimen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Eksperimen PHP</title>
+</head>
+<body>
+    <h1>Profil Mahasiswa</h1>
+    <hr>
+    <?php
+echo"Nama : Linda Safitri<br>";
+echo"NIM : 240631100048<br>";
+echo"Universitas : Universitas Trunodjoyo Madura<br>";
+echo"Hobi : Badmintoon<br>";
+echo"Cita-Cita : Guru<br>";
+echo"Alasan belajar web programming: Ingin membuat website interaktif<br>";
+?>
+</body>
+</html>
