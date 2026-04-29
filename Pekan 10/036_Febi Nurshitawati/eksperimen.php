cfdd61c2e82781d8df389c272c20946a93de566f
fathiyahaisyahnajiyah <fathiyahaisyahnajiyah@gmail.com>
2026-04-29 09:05:48 +0700

    Create eksperimen.php

---
+++ Pekan 10/036_Febi Nurshitawati/eksperimen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+    <h1>Data diri Mahasiswa</h1>
+    <hr>
+
+    <?php
+        echo "Nama : Febi Nurshitawati <br>";
+        echo "Universitas : Universitas Trunojoyo Madura <br>";
+        echo "Hobi : Membaca dan menulis <br>";
+        echo "Cita-cita : Guru <br>";
+    ?>
+
+</body>
+</html>
