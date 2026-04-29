a54e8c6f1a201a2b1df49a5ebb5fd6cce8d215b8
fathiyahaisyahnajiyah <fathiyahaisyahnajiyah@gmail.com>
2026-04-29 08:36:35 +0700

    Create Eksperimen.php

---
+++ Pekan 10/039_Aisyah Najiyah Fathiyah/Eksperimen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+<h1>Data diri mahasiswa</h1>
+<hr>
+
+<?php
+echo "Nama : Maulida <br>";
+echo "Universitas : Universitas Trunojoyo Madura <br>";
+echo "Hobi : Membaca dan Menulis <br>";
+echo "Cita-cita : Guru <br>";
+echo "Umur : 19 tahun <br>";
+?>
+
+</body>
+</html>
