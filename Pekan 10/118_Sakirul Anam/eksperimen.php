a1754fe41852304e702ea81410157b21dd85e7fc
118_Sakirul Anam <137684824+Nerveal@users.noreply.github.com>
2026-04-29 08:29:18 +0700

    Add files via upload

---
+++ Pekan 10/118_Sakirul Anam/eksperimen.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2>Profil Mahasiswa</h2>";
+
+echo "<hr>";
+
+echo "Nama: Sakirul Anam <br>";
+echo "Universitas: Trunojoyo Madura <br>";
+echo "Hobi: Tidur <br>";
+echo "Cita-cita: Guru <br>";
+echo "Alasan belajar web programming: Agar bisa membuat web <br>";
+
+?>
---
+++ Pekan 10/118_Sakirul Anam/halaman.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Latihan PHP</title>
+</head>
+
+<body>
+
+    <h1>Belajar PHP</h1>
+        
+    <h2>Mengenal Server Side Programming</h2>
+
+    <p>PHP adalah bahasa pemrograman yang berjalan di sisi server untuk membuat website menjadi lebih dinamis.</p>
+
+    <?php
+    
+    echo "Hari ini saya belajar server side programming <br>";
+    echo "Latihan ini membantu saya memahami sintaks dasar PHP <br>";
+    echo "Semangat belajar coding!";
+
+    ?>
+
+</body>
+
+</html>
