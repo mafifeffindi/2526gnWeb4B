9c2ecb83a552682230a619b4a6ad31530aa4488e
ZAIDAN009671 <akhzaidanadima@gmail.com>
2026-04-29 08:44:21 +0700

    Create halaman.php with PHP and HTML content
    Add a basic PHP and HTML example page.

--- Pekan 10/057_Zaidan/halaman.php
+++ Pekan 10/057_Zaidan/halaman.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman PHP</title>
+</head>
+<body>
 
+<h1>Belajar PHP Dasar</h1>
+
+<p>Ini adalah contoh kombinasi HTML dan PHP dalam satu file.</p>
+
+<?php
+echo "Baris 1: Halo, saya Nasrul sedang belajar PHP <br>";
+echo "Baris 2: PHP adalah bahasa server-side <br>";
+echo "Baris 3: PHP bisa digabung dengan HTML <br>";
+echo "Baris 4: kenapa nyak <br>";
+?>
+
+</body>
+</html>
