b65389a5afb3ddbffbc658bae764b1df0a62946f
rosdawananingrum-ux <rosdawananingrum@gmail.com>
2026-04-29 09:13:54 +0700

    Create halaman.php

---
+++ Pekan 10/038_Rosdawana Auril Ningrum/halaman.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Latihan PHP</title>
+</head>
+
+<body>
+
+    <h1>Belajar PHP</h1>
+
+    <?php
+        echo "Hari ini saya belajar server side programming";
+    ?>
+
+</body>
+
+</html>
