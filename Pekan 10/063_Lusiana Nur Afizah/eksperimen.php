8e47df4003a4f995b74434f7966d382a7c4f2772
lusiananurafizah21-star <lusiananurafizah21@gmail.com>
2026-04-29 08:39:36 +0700

    Create eksperimen.php

---
+++ Pekan 10/063_Lusiana Nur Afizah/eksperimen.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+<h1>Data Diri Mahasiswa</h1>
+<hr>
+<?php
+echo "Nama : Lusiana <br>";
+echo "Universitas : Universitas Contoh <br>";
+echo "Hobi : Membaca dan Menulis <br>";
+echo "Cita-cita : Menjadi Programmer <br>";
+echo "Alasan Belajar Web Programming : Ingin membuat website sendiri <br>";
+?>
+</body>
+</html>
