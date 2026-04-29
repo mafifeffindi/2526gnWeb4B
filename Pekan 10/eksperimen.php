90b2422cc78d746b6baaf05da134a46c428ddc38
mariskasafitri <mariskasftr@gmail.com>
2026-04-29 08:27:26 +0700

    Mariska Safitri 240631100052

---
+++ Pekan 10/eksperimen.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h2>Profil Saya</h2>";
+echo "<hr>";
+
+echo "Nama : Mariska Safitri <br>";
+echo "Universitas : Universitas Trunojoyo Madura <br>";
+echo "Hobi : Memasak dan Badminton <br>";
+echo "Cita-cita : Dokter <br>";
+echo "Alasan belajar web programming : Ingin membuat website sendiri <br>";
+?>
