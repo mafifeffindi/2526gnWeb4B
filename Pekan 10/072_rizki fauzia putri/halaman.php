deb9f760f078d0fd24e3000f3127520512682449
20rizkyfauziaputri <20rizkyfauziaputri@gmail.com>
2026-04-29 08:50:49 +0700

    Create halaman.php

---
+++ Pekan 10/072_rizki fauzia putri/halaman.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HalamanPHP</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG</h1>
+    <p>Ini adalah halaman sedrehana gabungan HTML dan PHP.</P>
+    <?php
+    echo "nama: rizki fauzia<br>";
+    echo "nim: 240631100072<br>";
+    echo "semangat belajar php<br>";
+    ?>
+    </body>
+    </html>
