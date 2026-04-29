c372cd590e5eccc1b2a2ddecb8367dd2b85c683f
ZAIDAN009671 <akhzaidanadima@gmail.com>
2026-04-29 08:42:52 +0700

    Update eksperimen.php

--- Pekan 10/057_Zaidan/eksperimen.php
+++ Pekan 10/057_Zaidan/eksperimen.php
@@ -1,1 +1,16 @@
+<?php
+$nama = "Akh Zaidan Adima";
+$univ = "Universitas Trunodjoyo Madura";
+$hobi = "Memamncing";
+$cita = "Pemilik MBG";
+$alasan = "Agar SUKSES DUNIA AKHIRAT";
 
+echo "<h2>Profil Mahasiswa Informatika</h2>";
+echo "<hr>";
+
+echo "<b>Nama:</b> " . $nama . "<br>";
+echo "<b>Universitas:</b> " . $univ . "<br>";
+echo "<b>Hobi:</b> " . $hobi . "<br>";
+echo "<b>Cita-cita:</b> " . $cita . "<br>";
+echo "<b>Alasan Belajar:</b> " . $alasan;
+?>
