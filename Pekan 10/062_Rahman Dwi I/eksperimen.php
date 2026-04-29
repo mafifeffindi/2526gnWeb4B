d44b5fb3eb19c69fb6f2f398a16d4be450e588ad
RemixSheep <rahmandwi660@gmail.com>
2026-04-29 08:26:19 +0700

    Create eksperimen.php

---
+++ Pekan 10/062_Rahman Dwi I/eksperimen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+<h1>Data Diri Mahasiswa</h1>
+<hr>
+
+<?php
+echo "Nama: Rahman Dwi I <br>";
+echo "Universitas: Universitas Trunojoyo Madura <br>";
+echo "Hobi: Gaming dan Coding <br>";
+echo "Cita-cita: Menjadi Web Developer <br>";
+echo "Alasan belajar Web Programming: Untuk membuat aplikasi dan website sendiri <br>";
+?>
+
+</body>
+</html>
