1c8da1b9e76026d954ee8a5817035f36a721f910
thebosswarior-cpu <thebosswarior@gmail.com>
2026-04-29 12:57:41 +0700

    Add files via upload

---
+++ Pekan 10/060_moh.iqbaldaymumy/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+<h1>Data diri mahasiswa</h1>
+
+<?php
+echo "nama : daymumy<br>";
+echo "Universitas Trunodjoyo Madura<br>";
+echo "Nim : 240631100060<r>";
+echo "Sosial Media<br>";
+echo "Instagram : dymumy<br>";
+?>
+
+</body>
+</html>
