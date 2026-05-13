e407007433e1b310c38ea6304098787618d0e37f
fathiyahaisyahnajiyah <fathiyahaisyahnajiyah@gmail.com>
2026-05-13 08:12:00 +0700

    Create functions.php

---
+++ Pekan 11/039_Aisyah Najiyah Fathiyah/Revisi project mahasiswa/functions.php
@@ -0,0 +1,11 @@
+<?php
+function formatNama($nama) {
+    return strtoupper(trim($nama));
+}
+
+function validasiEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+$jurusan_list = ["Informatika", "Sistem Informasi", "Teknik Komputer"];
+?>
