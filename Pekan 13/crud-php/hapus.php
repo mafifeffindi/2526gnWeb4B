a0eac01d6ad854995002c3ed43732332e1f5de66
Muhammad Al-Rhosit <muhammadalrhosit@gmail.com>
2026-05-20 08:10:52 +0700

    Delete Pekan 13/crud-php directory

--- Pekan 13/crud-php/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$id = $_GET['id'];
-
-mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE id='$id'");
-header("Location: index.php");
-
-?>
--- Pekan 13/crud-php/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "kampus-db");
-
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
