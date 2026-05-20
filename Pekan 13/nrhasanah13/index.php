3f8e93941db0ada89f3b4f4edf57ae644a183ae8
nurhasanah067 <nurhasanah200306@gmail.com>
2026-05-20 08:20:01 +0700

    Add files via upload

---
+++ Pekan 13/nrhasanah13/index.php
@@ -0,0 +1,47 @@
+<?php include 'koneksi.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Mahasiswa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Data Mahasiswa</h2>
+
+<a href="tambah.php">+ Tambah Data</a>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>No</th>
+        <th>Nama</th>
+        <th>NIM</th>
+        <th>Jurusan</th>
+        <th>Aksi</th>
+    </tr>
+
+<?php
+$no = 1;
+$data = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+while($d = mysqli_fetch_array($data)){
+?>
+
+<tr>
+    <td><?php echo $no++; ?></td>
+    <td><?php echo $d['nama']; ?></td>
+    <td><?php echo $d['nim']; ?></td>
+    <td><?php echo $d['jurusan']; ?></td>
+    <td>
+        <a href="edit.php?id=<?php echo $d['id']; ?>">Edit</a>
+        |
+        <a href="hapus.php?id=<?php echo $d['id']; ?>">Hapus</a>
+    </td>
+</tr>
+
+<?php } ?>
+
+</table>
+
+</body>
+</html>
