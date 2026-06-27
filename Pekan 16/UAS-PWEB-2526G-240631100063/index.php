3c44cd40b6666fa1fca67405b5e1d29c7d70e1fb
lusiananurafizah21-star <lusiananurafizah21@gmail.com>
2026-06-27 22:12:44 +0700

    Create index.php

---
+++ Pekan 16/UAS-PWEB-2526G-240631100063/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sistem Informasi Buku</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="navbar">
+    <div class="logo">📚 Sistem Buku</div>
+    <div class="menu">
+        <a href="index.php">Home</a>
+        <a href="tambah.php">Tambah</a>
+        <a href="daftar.php">Daftar</a>
+    </div>
+</div>
+
+<div class="container">
+    <div class="card" style="text-align:center;">
+        <h1>Sistem Informasi Data Buku</h1>
+        <p>Kelola data buku dengan mudah dan cepat</p>
+
+        <a href="tambah.php" class="btn btn-green">Tambah Data</a>
+        <a href="daftar.php" class="btn btn-blue">Lihat Data</a>
+    </div>
+</div>
+
+<div class="footer">
+    © 2026 Sistem Informasi Buku
+</div>
+
+</body>
+</html>
