732e3f46f4172db3f5f6fb561ee0b0958437561c
RemixSheep <rahmandwi660@gmail.com>
2026-06-27 13:02:03 +0700

    Create navbar.php
    Membuat Bar Navigasi untuk lebih memudahkan tampilan

---
+++ Pekan 16/UAS-PWEB-2526G-240631100062/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">To-Do List</a>
+
+        <div>
+            <a href="index.php" class="btn btn-light">Home</a>
+            <a href="tambah.php" class="btn btn-light">Tambah</a>
+            <a href="daftar.php" class="btn btn-light">Daftar</a>
+        </div>
+
+    </div>
+</nav>
