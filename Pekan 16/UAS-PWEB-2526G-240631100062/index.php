c8441467eb805b5637ffadacf00a0eaeee4555c1
RemixSheep <rahmandwi660@gmail.com>
2026-06-27 10:29:32 +0700

    Create index.php
    Membuat halaman Home

---
+++ Pekan 16/UAS-PWEB-2526G-240631100062/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	
+	<body>
+		<?php include 'navbar.php'; ?>
+		<div class="container">
+			<div class="card">
+				<div class="card-body">
+					<h2>Aplikasi To-Do List</h2>
+					<p>
+					Aplikasi sederhana untuk mencatat tugas harian menggunakan PHP Native dan MySQL.
+					</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
