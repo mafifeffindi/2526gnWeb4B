8b8715bfd515c06053cfe90a0f43cbd23309cab6
rosdawananingrum-ux <rosdawananingrum@gmail.com>
2026-06-28 20:21:22 +0700

    Add form for adding student data tambah.data

---
+++ tambah.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tambah Mahasiswa</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h2>Tambah Data Mahasiswa</h2>
+
+<form action="simpan.php" method="POST">
+
+NIM <br>
+<input type="text" name="nim"><br><br>
+
+Nama <br>
+<input type="text" name="nama"><br><br>
+
+Jurusan <br>
+<input type="text" name="jurusan"><br><br>
+
+Angkatan <br>
+<input type="number" name="angkatan"><br><br>
+
+<input type="submit" value="Simpan">
+
+</form>
+
+</body>
+</html>
