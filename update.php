79930fe962daf8afe4c03c72c5844f5ac35a1edd
rosdawananingrum-ux <rosdawananingrum@gmail.com>
2026-06-28 20:29:04 +0700

    Add update.php for updating student records

---
+++ update.php
@@ -0,0 +1,18 @@
+<?php
+include "config/koneksi.php";
+
+$id=$_POST['id'];
+$nim=$_POST['nim'];
+$nama=$_POST['nama'];
+$jurusan=$_POST['jurusan'];
+$angkatan=$_POST['angkatan'];
+
+mysqli_query($conn,"UPDATE mahasiswa SET
+nim='$nim',
+nama='$nama',
+jurusan='$jurusan',
+angkatan='$angkatan'
+WHERE id='$id'");
+
+header("Location: daftar.php");
+?>
